d9d84588d70df6b7d1f0c7356e36a12231290459
Marielly Blank <mblank@c1r16s10.42lisboa.com>
2022-11-08 16:37:42 +0000

    ft_bzero ainda sem funcionar. precsa ser melhorado o main

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: mblank <mblank@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:26:09 by mblank            #+#    #+#             */
-/*   Updated: 2022/11/08 16:09:26 by mblank           ###   ########.fr       */
+/*   Updated: 2022/11/08 16:36:44 by mblank           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,35 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*ptr;
-	size_t			i;
+	size_t	i;
 
-	ptr = (unsigned char *)s;
 	i = 0;
-	while (n < i)
+	while (i < n)
 	{
-		*ptr = '\0';
-		ptr++;
+		*(unsigned char *)(s + i) = '\0'; //ha necessidade de estudar essa estrutura de ptr
+		i++;
 	}
 }
-//solucao:usar memset '\0'!
-#include <stdlib.h>
+
+//solucao possivel vista em mais de um lugar :usar memset '\0'!
+//mas para isso, precisa botar makefile pra funcionar
+/* #include <stdio.h>
 #include <string.h>
 
 int main( void )
   {
-    char buffer[80];
+    char str[] = "abcd";
 
-    ft_bzero( buffer, 80 );
-    return EXIT_SUCCESS;
-  }
+    ft_bzero( str, 2 );
+    printf("%s", str);
+  } */
+//teste abaixo ainda nao esta bom, dda stack smashing detected
+#include <stdio.h>
+
+int	main(void)
+{
+	size_t n = 5;
+	char a[] = "cinc";
+	ft_bzero(a + 5, 3*sizeof(char)); //precisa verificar se esta correto chamar a afuncao assim
+	printf("%s", a);
+}
